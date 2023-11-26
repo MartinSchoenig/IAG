b02cc36ddacfce311ad2acb273514011a0825914
MartinSchoenig <martinschoenig19@gmail.com>
2023-11-26 17:48:20 -0300

    added feedback functionality

--- Oblig2/app.py
+++ Oblig2/app.py
@@ -1,5 +1,16 @@
 import streamlit as st
 from transformers import T5ForConditionalGeneration, T5TokenizerFast
+import pandas as pd
+import os
+
+def save_feedback(text, paraphrase, csv_filename='feedback.csv'):
+    if not os.path.exists(csv_filename):
+        df = pd.DataFrame(columns=['Text', 'Paraphrase'])
+        df.to_csv(csv_filename, index=False)
+
+    feedback_df = pd.DataFrame({'Text': [text], 'Paraphrase': [paraphrase]})
+    df = pd.concat([pd.read_csv(csv_filename), feedback_df], ignore_index=True)
+    df.to_csv(csv_filename, index=False)
 
 def paraphrase(text, do_sampling, num_samples):
     model_dir = './models/outputs_4'
@@ -26,8 +37,14 @@ def paraphrase(text, do_sampling, num_samples):
         paraphrased_text = tokenizer.decode(encoded_output, skip_special_tokens=True)
         return paraphrased_text
 
-st.set_page_config(page_title='✏️🌪️ Paraphrasing App', page_icon='🌪️', layout='centered', initial_sidebar_state='auto')
-st.title('✏️🌪️ Paraphrasing App')
+state = st.session_state
+if 'paraphrased_text' not in state:
+    state.paraphrased_text = None
+if 'feedback' not in state:
+    state.feedback = None
+
+st.set_page_config(page_title='✏️🌪️ Paraphrasing', page_icon='🌪️', layout='centered', initial_sidebar_state='auto')
+st.title('✏️🌪️ Paraphrasing')
 
 txt_input = st.text_area('Enter your text', '', height=50)
 
@@ -36,18 +53,25 @@ num_samples = 1
 if do_sampling:    
     num_samples = st.number_input('Number of Samples', min_value=1, max_value=10, value=1)
 
-result = []
-with st.form('summarize_form', clear_on_submit=True):
-    submitted = st.form_submit_button('Submit')
-    if submitted:
-        with st.spinner('Paraphrasing...'):
-            response = paraphrase(txt_input, do_sampling, num_samples)
-            result.append(response)
+if st.button('Paraphrase'):
+    with st.spinner('Paraphrasing...'):
+        response = paraphrase(txt_input, do_sampling, num_samples)
+        state.paraphrased_text = response
 
-if len(result):
+if state.paraphrased_text:
     st.subheader('Paraphrased Text:')
     if do_sampling:
-        for i, paraphrase_text in enumerate(result[0]):
-            st.write(f"Sample {i + 1}: {paraphrase_text}")
+        for i, paraphrase_text in enumerate(state.paraphrased_text):
+            st.success("Sample {}: {}".format(i+1, paraphrase_text))
     else:
-        st.write(result[0])
+        st.success(state.paraphrased_text)
+
+    state.feedback = st.radio("Help us improve our model! Was the resulting (first one if sampled) paraphrase good or bad?", options=['Good', 'Bad'], index=None)
+    submit_feedback = st.button('Submit Feedback')
+    if submit_feedback:
+        if state.feedback == 'Good':
+            if do_sampling:
+                save_feedback(txt_input, state.paraphrased_text[0])
+            else:
+                save_feedback(txt_input, state.paraphrased_text)
+        
